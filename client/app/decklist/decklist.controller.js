5f98b1b811b7493b3fe552c617fbf1093c20ce4c
Victor Gil Borrego <victor.gil@lbigroup.es>
2016-08-09 15:44:42 +0200

    added sortable tables

--- client/app/decklist/decklist.controller.js
+++ client/app/decklist/decklist.controller.js
@@ -5,10 +5,15 @@
 class DecklistComponent {
   constructor($http, Auth) {
     this.$http = $http;
-    
+
     this.isLoggedIn = Auth.isLoggedIn;
     this.isAdmin = Auth.isAdmin;
     this.getCurrentUser = Auth.getCurrentUser;
+
+    //Sorting
+    this.sortType     = 'name'; // set the default sort type
+    this.sortReverse  = false;  // set the default sort order
+    this.searchFish   = '';     // set the default search/filter term
 
 
     this.decks = [];
